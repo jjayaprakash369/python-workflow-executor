f6b6845bab332b97ab9e43d9d95ed188d00971c5
Jayaprakash Jayabalan <jayabalj@in.ibm.com>
2018-11-05 12:17:15 +0530

    Initial Commit

---
+++ python-flow-executor/runflow.py
@@ -0,0 +1,101 @@
+'''
+Created on 04-Nov-2018
+
+@author: JayaprakashJayabalan
+'''
+import sys
+import json
+import logging
+import subprocess
+import shlex
+import time
+
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+
+input_flow_file='flow-ipis.json'
+
+runtime_environment='dev'
+
+#Parsing the input flow json file and loading it into global flow_dict dictionary 
+logging.info('Loading the flow from input flow json')
+try:
+    with open(input_flow_file, 'r') as f:
+        flow_dict = json.load(f)
+except Exception as e:
+    logging.exception("Exception occurred in loading the input flow file")
+    sys.exit(101)
+
+logging.info('Flow loaded successfully')
+
+
+def executeFlow(flowName):
+    logging.info("Starting the flow execution ")
+    logging.debug("DEBUG:: Finding the first-step")
+    __intial_step__= flow_dict['StartsWith']
+    __not_final_step__=True
+    __current_step_name__=__intial_step__
+    __current_step__=flow_dict[__current_step_name__]
+    while(__not_final_step__):
+        logging.info("Initiating the step %s",__current_step_name__)
+        __response_status__=executeStep(__current_step_name__,__current_step__)
+        if __response_status__ != 0:
+            logging.error("Error occurred in the step execution. Terminating the flow execution")
+            sys.exit(__response_status__)
+        __next_step_name__=__current_step__['NEXTSTATE']
+        if __next_step_name__ == 'ENDSWITHTHIS' :
+            __not_final_step__=False
+        else:
+            __current_step__=flow_dict[__next_step_name__]
+            __current_step_name__=__next_step_name__
+
+def runShell(_command_):
+    #_response_code_=subprocess.call(shlex.split('./test.sh param1 param2'))
+    print("Execution shell %s",_command_)
+    if _command_ == 'GLUE_STEP_JOB2':
+        return 101
+    return 0
+
+
+def executeStep(stepName, step):
+    logging.info("Starting the step execution %s",stepName)
+    _task_type_=step['TYPE']
+    _resource_name_=step['RESOURCE']
+    _parameters_=step['PARAMETER']
+    _retries=step['RETRIES']
+    _waitime=step['WAITTIME']
+    _retry_count_=0
+    _run_command_=''
+    if _task_type_ == 'ECS':
+        logging.info('Decoding the parameter for the ECS task execution')
+        _run_command_=stepName
+    elif _task_type_ == 'GLUE':
+        logging.info('Decoding the parameter for the GLUE execution')
+        _run_command_=stepName
+    
+    
+    _execution_status_=True
+    while _execution_status_ and _retry_count_ <= _retries:
+        _response_code_=runShell(_run_command_)
+        if _response_code_ == 0:
+            logging.info('Step %s execution completed successfully',stepName)
+            _execution_status_=False
+        else:
+            logging.info('Step %s execution Failed', stepName)
+            logging.info('Waiting for %i sec to retry',_waitime)
+            time.sleep(_waitime)
+            _retry_count_ += 1
+            if _retry_count_ <= _retries:
+                logging.info('Retrying %i of %i',_retry_count_,_retries)
+    if _execution_status_:
+        logging.error('Step %s execution failed. Error Code %s',stepName,_response_code_)
+        return _response_code_
+    else:
+        return _response_code_
+
+
+def main():
+    executeFlow('IPIS_REPORT')
+
+if __name__== "__main__":
+    main()
